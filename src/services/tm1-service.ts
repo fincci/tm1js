1a8bd5172075535e3068f4d6682fda0c8661ce54
João Pedro Dias Caparroz <jpkook2011@gmail.com>
2024-04-27 17:27:04 -0300

    identation fix

--- src/services/tm1-service.ts
+++ src/services/tm1-service.ts
@@ -9,45 +9,45 @@ import { CellService } from './cell-service'
 import ServerService from './server-service'
 
 class TM1Service {
-  private _rest: RestService
-  public cubes: CubeService
-  public dimensions: DimensionService
-  public processes: ProcessService
-  public chores: ChoreService
-  public views: ViewService
-  public security: SecurityService
-  public cells: CellService
-  public server: ServerService
-  constructor(rest: RestService) {
-    this._rest = rest
-    this.cubes = new CubeService(this._rest)
-    this.dimensions = new DimensionService(this._rest)
-    this.processes = new ProcessService(this._rest)
-    this.chores = new ChoreService(this._rest)
-    this.views = new ViewService(this._rest)
-    this.security = new SecurityService(this._rest)
-    this.cells = new CellService(this._rest)
-    this.server = new ServerService(this._rest)
-  }
+	private _rest: RestService
+	public cubes: CubeService
+	public dimensions: DimensionService
+	public processes: ProcessService
+	public chores: ChoreService
+	public views: ViewService
+	public security: SecurityService
+	public cells: CellService
+	public server: ServerService
+	constructor(rest: RestService) {
+		this._rest = rest
+		this.cubes = new CubeService(this._rest)
+		this.dimensions = new DimensionService(this._rest)
+		this.processes = new ProcessService(this._rest)
+		this.chores = new ChoreService(this._rest)
+		this.views = new ViewService(this._rest)
+		this.security = new SecurityService(this._rest)
+		this.cells = new CellService(this._rest)
+		this.server = new ServerService(this._rest)
+	}
 
-  static async connect(config: RestConfig) {
-    const _rest = new RestService(config)
-    await _rest.startSession(
-      config.user,
-      config.password,
-      config.namespace,
-      config.impersonate
-    )
-    return new TM1Service(_rest)
-  }
+	static async connect(config: RestConfig) {
+		const _rest = new RestService(config)
+		await _rest.startSession(
+			config.user,
+			config.password,
+			config.namespace,
+			config.impersonate
+		)
+		return new TM1Service(_rest)
+	}
 
-  async logout() {
-    return this._rest.logout()
-  }
+	async logout() {
+		return this._rest.logout()
+	}
 
-  get version() {
-    return this._rest.version
-  }
+	get version() {
+		return this._rest.version
+	}
 }
 
 export { TM1Service }
